c88170a289d4d8a023e26d4f8cefaf0c29f38846
Aman <amanyadav9675maa@gmail.com>
2024-07-11 17:47:20 +0530

    First commit

---
+++ shopapp redux/src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className='relative'>
+      <div className='spinner absolute left-[10]'></div>
+    </div>
+  )
+}
+
+export default Spinner
